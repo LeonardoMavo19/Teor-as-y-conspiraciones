44c3b03665f95b9f83852347bfb94be90737ebd1
LeonardoMavo <teadera77@gmail.com>
2023-01-06 20:05:11 -0400

    Actualizado
    Efecto contenedor

---
+++ Function.js
@@ -0,0 +1,33 @@
+function mostrar(){
+    document.getElementById('texto').style.display= 'block';
+}
+
+function ocultar(){
+    document.getElementById('texto').style.display= 'none';
+}
+
+function mostrardos(){
+    document.getElementById('texto-dos').style.display= 'block'
+}
+function ocultardos(){
+    document.getElementById('texto-dos').style.display= 'none'
+}
+
+function mostrartres(){
+    document.getElementById('texto-tres').style.display= 'block'
+}
+function ocultartres(){
+    document.getElementById('texto-tres').style.display= 'none'
+}
+function mostrarcuatro(){
+    document.getElementById('texto-cuatro').style.display= 'block'
+}
+function ocultarcuatro(){
+    document.getElementById('texto-cuatro').style.display= 'none'
+}
+function mostrarcinco(){
+    document.getElementById('texto-cinco').style.display= 'block'
+}
+function ocultarcinco(){
+    document.getElementById('texto-cinco').style.display= 'none'
+}
